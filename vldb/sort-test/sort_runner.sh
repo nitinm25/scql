d8724c82f68db80a2b395d2336bf687c7c73a411
Nitin <nitinmathai2000@gmail.com>
2024-11-30 05:07:15 +0530

    Update runner

--- vldb/sort-test/sort_runner.sh
+++ vldb/sort-test/sort_runner.sh
@@ -1,4 +1,18 @@
+echo -e "Sort: 1000\n"
+
 bazel run //vldb/sort-test:sort -c opt -- --numel=1000
+
+echo -e "\n\n\n\n"
+echo -e "Sort: 10000\n"
+
 bazel run //vldb/sort-test:sort -c opt -- --numel=10000
+
+echo -e "\n\n\n\n"
+echo -e "Sort: 100000\n"
+
 bazel run //vldb/sort-test:sort -c opt -- --numel=100000
+
+echo -e "\n\n\n\n"
+echo -e "Sort: 1000000\n"
+
 bazel run //vldb/sort-test:sort -c opt -- --numel=1000000
