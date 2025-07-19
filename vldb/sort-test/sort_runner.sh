331bf060d4d5fc37af3c3f358aef16621d946fe6
Nitin <nitinmathai2000@gmail.com>
2025-07-19 18:21:04 -0400

    Update sort_runner

--- vldb/sort-test/sort_runner.sh
+++ vldb/sort-test/sort_runner.sh
@@ -1,18 +1,23 @@
-echo -e "Sort: 1000\n"
-
-bazel run //vldb/sort-test:sort -c opt -- --numel=1000
-
-echo -e "\n\n\n\n"
-echo -e "Sort: 10000\n"
-
-bazel run //vldb/sort-test:sort -c opt -- --numel=10000
-
-echo -e "\n\n\n\n"
 echo -e "Sort: 100000\n"
+bazel run //vldb/sort-test:sort_sbk -c opt -- --numel=100000
+bazel run //vldb/sort-test:sort_sbk -c opt -- --numel=100000
+bazel run //vldb/sort-test:sort_sbk -c opt -- --numel=100000
+bazel run //vldb/sort-test:sort_sbk_valid -c opt -- --numel=100000
+bazel run //vldb/sort-test:sort_sbk_valid -c opt -- --numel=100000
+bazel run //vldb/sort-test:sort_sbk_valid -c opt -- --numel=100000
 
-bazel run //vldb/sort-test:sort -c opt -- --numel=100000
-
-echo -e "\n\n\n\n"
 echo -e "Sort: 1000000\n"
+bazel run //vldb/sort-test:sort_sbk -c opt -- --numel=1000000
+bazel run //vldb/sort-test:sort_sbk -c opt -- --numel=1000000
+bazel run //vldb/sort-test:sort_sbk -c opt -- --numel=1000000
+bazel run //vldb/sort-test:sort_sbk_valid -c opt -- --numel=1000000
+bazel run //vldb/sort-test:sort_sbk_valid -c opt -- --numel=1000000
+bazel run //vldb/sort-test:sort_sbk_valid -c opt -- --numel=1000000
 
-bazel run //vldb/sort-test:sort -c opt -- --numel=1000000
+echo -e "Sort: 10000000\n"
+bazel run //vldb/sort-test:sort_sbk -c opt -- --numel=10000000
+bazel run //vldb/sort-test:sort_sbk -c opt -- --numel=10000000
+bazel run //vldb/sort-test:sort_sbk -c opt -- --numel=10000000
+bazel run //vldb/sort-test:sort_sbk_valid -c opt -- --numel=10000000
+bazel run //vldb/sort-test:sort_sbk_valid -c opt -- --numel=10000000
+bazel run //vldb/sort-test:sort_sbk_valid -c opt -- --numel=10000000
